e0dd340c9d19e282b1079b91b7c812184e84318e
genyklemberg <y.potupa@gmail.com>
2016-08-02 19:44:32 +0300

    'v02_fonts_fixed_6'

---
+++ scripts/vendor/main.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+ 
+    
+//     $("a.transition_to").click(function(event){
+//         event.preventDefault();
+//         linkLocation = this.href;
+//         $("body").removeClass("loaded");     
+//         setTimeout(redirectPage, 450);
+//     });
+          
+//     function redirectPage() {
+//         window.location = linkLocation;
+//     }
+// });
